577c3098799524891f6d3b0f7ee2abe0102f5489
bharagy <145537623+bharagy@users.noreply.github.com>
2023-11-27 12:39:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
+chmod +x build.sh
 ./build.sh
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD 
 docker tag myreactimg dhividhivya/dev
 docker push dhividhivya/dev
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
+chmod +x build.sh
 ./build.sh
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker tag myreactimg dhividhivya/prod
